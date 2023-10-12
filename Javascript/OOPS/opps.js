2f54926e4a3dc981bfc58db2ed99947634646cb8
adfar-136 <adfarrasheed136@gmail.com>
2023-10-12 22:54:07 +0530

    donnnne

--- Javascript/OOPS/opps.js
+++ Javascript/OOPS/opps.js
@@ -22,15 +22,43 @@
 //     firstName,
 //     lastName,
 //     age,
-//     getBirthYear,
+//     // getBirthYear:CreateUser.commonMethods.getBirthYear
+    
 //   };
 //   return user;
 // }
+// // CreateUser.commonMethods = {
+// //     getBirthYear: function () {
+// //         return new Date().getFullYear() - this.age;
+// //       } 
+// // }
 
 // const user1 = CreateUser("Adfar","Rasheed",25);
 // const user2 = CreateUser("Aryan","Jain",24);
 
-
-function greet(){
-    console.log("Adfar")
+function CreateUser(firstName,lastName,age){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age
 }
+CreateUser.prototype.getBirthYear = 
+    function () {
+    return new Date().getFullYear() - this.age;
+
+} 
+CreateUser.prototype.getFullName = 
+function(){
+    return `${this.firstName} ${this.lastName}` ;
+}
+
+ const user1 =new CreateUser("Adfar","Rasheed",25);
+ const user2 =new CreateUser("Aryan","Jain",24);
+
+
+
+// function greet(){
+//     console.log(this)
+//     console.log("Adfar")
+    
+// }
+
