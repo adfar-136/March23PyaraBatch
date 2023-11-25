e9aeab97961030307e6b1e32e77181d0a3008a93
adfar-136 <adfarrasheed136@gmail.com>
2023-11-25 23:02:57 +0530

    done

--- React/pyarabatch/src/App.js
+++ React/pyarabatch/src/App.js
@@ -2,17 +2,11 @@ import About from "./Components/About"
 function App(){
   return (
     <div>
-     <About></About>
+      <About firstName="Adfar" age="25" lastName="Rashid"/>
+      <About firstName="Aryan" age="23" lastName="Jain"/>
+      <About firstName="Gurpreet" age="20" lastName="Singh"/>
     </div>
   )
 }
 export default App
 
-const Header = [1,2,3,4,5]
- const Main = (
-  <main>
-    <h1>Welcome to my website</h1>
-    <a href="/">Google</a>
-    <small>I am a mern instructor</small>
-  </main>
- )
---
+++ React/pyarabatch/src/Components/Contact.js
@@ -0,0 +1,8 @@
+function Contact(props){
+    return (
+        <div>
+            <h1>hello jinab {props.fName}</h1>
+        </div>
+    )
+}
+export default Contact;
