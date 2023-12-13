5901afadbb976bbe09ab991ab6b6c70d51778138
adfar-136 <adfarrasheed136@gmail.com>
2023-12-13 22:50:49 +0530

    done

--- React/pyarabatch/src/App.js
+++ React/pyarabatch/src/App.js
@@ -2,9 +2,7 @@ import About from "./Components/About"
 function App(){
   return (
     <div>
-      <About firstName="Adfar" age="25" lastName="Rashid"/>
-      <About firstName="Aryan" age="23" lastName="Jain"/>
-      <About firstName="Gurpreet" age="20" lastName="Singh"/>
+      <About firstName="Newton"/>
     </div>
   )
 }
---
+++ React/pyarabatch/src/Components/About.js
@@ -0,0 +1,35 @@
+//render()
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  constructor(){
+    console.log("constructor");
+    super();
+    this.state = {
+        count:0,
+        name:"Adfar"
+    }
+  }
+  static getDerivedStateFromProps(props,state){
+    console.log("getDerivedStateFromProps")
+    return {name:props.firstName}
+  }
+  componentDidMount(){
+    console.log("componentdidMount");
+    setTimeout(()=>{
+        this.setState({
+            count :this.state.count+1
+    })
+    },1000)
+  }
+  render() {
+    console.log("render")
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={()=>{this.setState({count:this.state.count+1})}}>Add One</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    )
+  }
+}
